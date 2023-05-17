427736efbc6d2d3280f248d061eb2e5b878503e2
dongyoonq <ut_community@naver.com>
2023-05-17 22:20:37 +0900

    feat : Add Homework 2023.05.17
    애니메이션, UI, Scene 과제 업로드

--- Assets/Homework/2023.05 17/Script/ButtonFire.cs
+++ Assets/Homework/2023.05 17/Script/ButtonFire.cs
@@ -8,16 +8,10 @@ public class ButtonFire : MonoBehaviour
     private GameObject bulletPrefab;
     [SerializeField]
     private Transform bulletPos;
-    [SerializeField]
-    private GameObject tank;
 
     public void Fire()
     {
-        if (!tank.GetComponent<Fire>().isActiveAndEnabled)
-        {
-            Instantiate(bulletPrefab, bulletPos.position, bulletPos.rotation);
-            Destroy(bulletPrefab, 5f);
-        }
-
+        Instantiate(bulletPrefab, bulletPos.position, bulletPos.rotation);
+        Destroy(bulletPrefab, 5f);
     }
 }
